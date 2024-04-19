8acc69c21ae2bb11c37c2249dbda626088b644e2
Hellen Poeschke <130069159+poeschkeh@users.noreply.github.com>
2024-04-19 10:45:31 -0300

    Calculadora-ContadorCaracteres

---
+++ Projetos-DW-JS/ExContador-Caracteres/script.js
@@ -0,0 +1,9 @@
+const inputText = document.querySelector("(input-texto)")
+const btnContar = document.querySelector("(btn-contar)")
+const divContador = document.querySelector("(text-contador)")
+
+function saudacao(){
+    console.log("Olá")
+}
+
+btnContar.addEventListener("click", saudacao)
