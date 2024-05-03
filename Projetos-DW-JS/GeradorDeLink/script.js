96aff58dafa2e567962daf9dc29d8c82c68e58b7
Hellen Poeschke <130069159+poeschkeh@users.noreply.github.com>
2024-05-03 09:56:42 -0300

    atualizaçãoGerador-Link

---
+++ Projetos-DW-JS/GeradorDeLink/script.js
@@ -0,0 +1,9 @@
+const inputTelefone = document.querySelector("[input]")
+const BtnLink = document.querySelector("[gerar-link-button]")
+const BtnSend = document.querySelector("[copiar-link-button]")
+
+function handleBtnLink(){
+    //console.log(evento.target.value)
+}
+
+BtnLink.addEventListener("click", handleBtnLink)
