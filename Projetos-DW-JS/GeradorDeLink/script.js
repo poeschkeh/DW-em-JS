3280b0edaf769700570fef500a093ae680ac980e
Hellen Poeschke <130069159+poeschkeh@users.noreply.github.com>
2024-05-16 14:40:30 -0300

    atualização

--- Projetos-DW-JS/GeradorDeLink/script.js
+++ Projetos-DW-JS/GeradorDeLink/script.js
@@ -1,9 +1,25 @@
-const inputTelefone = document.querySelector("[input]")
-const BtnLink = document.querySelector("[gerar-link-button]")
-const BtnSend = document.querySelector("[copiar-link-button]")
+let inputTelefone = document.querySelector("input")
+let btnLink = document.querySelector("gerar-link-button")
+let btnSend = document.querySelector("copiar-link-button")
+let linkTexto = document.querySelector("link")
+let mensagem = document.querySelector("mensagem")
+//let resultLink = document.querySelector("#resultLink")
+
+//Padrão para definir o formato do telefone (xx)xxxxx-xxxx
+const regex = /(\d{2})(\d{5})(\d{4})/;
 
 function handleBtnLink(){
-    //console.log(evento.target.value)
+    const numeroFormatado = numero.value.replace(regex, "($1) $2-$3")
+    console.log(numeroFormatado)
+    const link = "https://wa.me/" + numeroFormatado
+    console.log(link) 
 }
 
-BtnLink.addEventListener("click", handleBtnLink)
+function handleBtnSend(){
+    const linkNovaAba = "https://wa.me/" + numero.value
+    const win = window.open(linkNovaAba, "_blank")
+    win.focus()
+}
+
+btnLink.addEventListener("click", handleBtnLink)
+btnSend.addEventListener("click", handleBtnSend)
