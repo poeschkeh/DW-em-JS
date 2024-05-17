20e65a1e64587601c01eb4418b07b475a5d5fb9c
Hellen Poeschke <130069159+poeschkeh@users.noreply.github.com>
2024-05-17 00:52:29 -0300

    atualização-gerador-whats

--- Projetos-DW-JS/GeradorDeLink/script.js
+++ Projetos-DW-JS/GeradorDeLink/script.js
@@ -1,25 +1,52 @@
-let inputTelefone = document.querySelector("input")
-let btnLink = document.querySelector("gerar-link-button")
-let btnSend = document.querySelector("copiar-link-button")
-let linkTexto = document.querySelector("link")
-let mensagem = document.querySelector("mensagem")
-//let resultLink = document.querySelector("#resultLink")
+const Telefone = document.querySelector("input")
+const btnLink = document.querySelector("gerar-link-button")
+const btnSend = document.querySelector("copiar-link-button")
+const linkTexto = document.querySelector("linkTexto")
+const mensagem = document.querySelector("mensagem")
 
-//Padrão para definir o formato do telefone (xx)xxxxx-xxxx
-const regex = /(\d{2})(\d{5})(\d{4})/;
+const regex = /(\d{2})(\d{5})(\d{4})/; //Padrão para definir o formato do telefone (xx)xxxxx-xxxx
+
+Telefone.addEventListener("input", (evento) => {
+    console.log(evento.target.value)
+    evento.target.value = evento.target.value.replace(/\D/g, "");
+    evento.target.value = evento.target.value.replace(regex, "($1) $2-$3");
+    if (evento.target.value.length === 11){
+        evento.target.classList.add("telefoneValido");
+    } else {
+        evento.target.classList.remove("telefoneValido");
+    }
+});
+
+Telefone.addEventListener("focus", ()=> {
+    mensagem.textContent = " "
+})
 
 function handleBtnLink(){
-    const numeroFormatado = numero.value.replace(regex, "($1) $2-$3")
-    console.log(numeroFormatado)
+    console.log('Hellen')
+    const numeroFormatado = Telefone.value.replace(/(\d{2})(\d{5})-(\d{4})/, "($1) $2-$3");
     const link = "https://wa.me/" + numeroFormatado
-    console.log(link) 
+    linkTexto.textContent = link;
+    mensagem.textContent = "Clique no link para copiar";
+    linkTexto.style.border = "1px solid black";
+    linkTexto.style.borderRadius = "5px";
 }
 
 function handleBtnSend(){
-    const linkNovaAba = "https://wa.me/" + numero.value
-    const win = window.open(linkNovaAba, "_blank")
-    win.focus()
+    console.log('Hellen')
+    const linkNovaAba = "https://wa.me/" + numero.value;
+    const win = window.open(linkNovaAba, "_blank");
+    win.focus();
 }
 
-btnLink.addEventListener("click", handleBtnLink)
-btnSend.addEventListener("click", handleBtnSend)
+linkTexto.addEventListener("click", () => {
+    navigator.clipboard.writeText(linkTexto.textContent)
+        .then(() => {
+            mensagem.textContent = "Link copiado para área de transferência";
+        })
+        .catch((err) => {
+            console.error("Falha ao copiar p texto:", err);
+        });
+});
+
+btnLink.addEventListener("click", handleBtnLink);
+btnSend.addEventListener("click", handleBtnSend);
