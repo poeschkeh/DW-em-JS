9c01c5228593a785ea3fa3494f144080a0c2f1c7
Hellen Poeschke <130069159+poeschkeh@users.noreply.github.com>
2024-05-17 09:13:46 -0300

    atualizaçãoJS-geradorLinkWhats

--- Projetos-DW-JS/GeradorDeLink/script.js
+++ Projetos-DW-JS/GeradorDeLink/script.js
@@ -1,41 +1,49 @@
-const Telefone = document.querySelector("input")
-const btnLink = document.querySelector("gerar-link-button")
-const btnSend = document.querySelector("copiar-link-button")
-const linkTexto = document.querySelector("linkTexto")
-const mensagem = document.querySelector("mensagem")
+const telefone = document.querySelector("#input")
+const btnLink = document.querySelector("#gerar-link-button")
+const btnSend = document.querySelector("#copiar-link-button")
+const linkTexto = document.querySelector("#linkTexto")
+const mensagem = document.querySelector("#mensagem")
 
 const regex = /(\d{2})(\d{5})(\d{4})/; //Padrão para definir o formato do telefone (xx)xxxxx-xxxx
 
-Telefone.addEventListener("input", (evento) => {
+telefone.addEventListener("input", (evento) => {
     console.log(evento.target.value)
     evento.target.value = evento.target.value.replace(/\D/g, "");
     evento.target.value = evento.target.value.replace(regex, "($1) $2-$3");
-    if (evento.target.value.length === 11){
+    if (evento.target.value.length === 15){
         evento.target.classList.add("telefoneValido");
     } else {
         evento.target.classList.remove("telefoneValido");
     }
 });
 
-Telefone.addEventListener("focus", ()=> {
+telefone.addEventListener("focus", () => {
     mensagem.textContent = " "
+    linkTexto.textContent = " "
 })
 
 function handleBtnLink(){
-    console.log('Hellen')
-    const numeroFormatado = Telefone.value.replace(/(\d{2})(\d{5})-(\d{4})/, "($1) $2-$3");
-    const link = "https://wa.me/" + numeroFormatado
-    linkTexto.textContent = link;
-    mensagem.textContent = "Clique no link para copiar";
-    linkTexto.style.border = "1px solid black";
-    linkTexto.style.borderRadius = "5px";
+    const telefoneFormatado = telefone.value.replace(/\D/g, ""); //Remove o que não é número
+    if(telefoneFormatado.length === 11){
+        const link = "https://wa.me/55" + telefoneFormatado;
+        linkTexto.textContent = link;
+        mensagem.textContent = "Clique no link para copiar";
+        linkTexto.style.border = "1px solid black";
+        linkTexto.style.borderRadius = "5px";
+    } else {
+        alert("Erro. O número deve conter 11 dígitos!")
+    }
 }
 
 function handleBtnSend(){
-    console.log('Hellen')
-    const linkNovaAba = "https://wa.me/" + numero.value;
-    const win = window.open(linkNovaAba, "_blank");
-    win.focus();
+    const telefoneFormatado = telefone.value.replace(/\D/g, "");
+    if(telefoneFormatado.length === 11){
+        const linkNovaAba = "https://wa.me/" + telefoneFormatado;
+        const win = window.open(linkNovaAba, "_blank");
+        win.focus();
+    } else {
+        alert("Erro. O número deve conter 11 dígitos!")
+    }
 }
 
 linkTexto.addEventListener("click", () => {
@@ -44,7 +52,7 @@ linkTexto.addEventListener("click", () => {
             mensagem.textContent = "Link copiado para área de transferência";
         })
         .catch((err) => {
-            console.error("Falha ao copiar p texto:", err);
+            console.error("Falha ao copiar o texto:", err);
         });
 });
 
